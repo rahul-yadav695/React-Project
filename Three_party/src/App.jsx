59af8cb48372698cc325f3a7963a80837700725f
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-05-22 11:34:48 +0530

    ubdate code

--- Three_party/src/App.jsx
+++ Three_party/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react' 
-
+import { useState } from 'react'  
+import Product from './commponets/Product'
 
 function App() { 
 
   return (
     <>
-
+  <Product />
     </>
   )
 }
--- Three_party/src/commponets/Product.jsx
+++ Three_party/src/commponets/Product.jsx
@@ -1,1 +1,21 @@
-import React from "react";
+import React from 'react'
+import { product } from '../data/product'
+
+const Product = () => {
+    return (
+        <> 
+            <div className='grid grid-cols-3'>
+                {product.map((val)=>{
+                    return(
+                        <p>{val.name}</p>,
+                        <p>{val.category}</p>,
+                        <img className='w-100 h-50 object-cover mt-10 ' src={val.img} />
+                    )
+                })}
+            </div>
+         </>
+    )
+}
+
+export default Product
+
