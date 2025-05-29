cb2968d588b2d613a0b06213ed1c772a208a2fab
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-05-29 10:39:12 +0530

    edit the code

--- pretics/src/component/Todo.jsx
+++ pretics/src/component/Todo.jsx
@@ -4,17 +4,18 @@ import React, { useState } from 'react'
 const Todo = () => {
 
     const [name, setname] = useState('')
-    const [title, settitle] = useState('')
-    const [helpbox, sethelpbox] = useState('')
-    const [filterbton, setbutton] = useState('')
+    const [title , settitle] = useState('')
+    const [description , setdescription] = useState('')
+    const [filterbton, setbutton] = useState(null)
 
-    const handleSubmit = () => {
-        setbutton({
+    const addtofilterarr = () => {
+        setbutton([
             name,
             title,
-            helpBox: helpbox
-        }); 
-    };
+            description,
+            filterbton
+        ])
+    } 
     return (
         <>
             <div className='ml-50 mt-50 border-2 w-60 p-5'>
@@ -23,15 +24,16 @@ const Todo = () => {
                 <label>Enter Title:--</label>
                 <input onChange={(e) => settitle(e.target.value)} value={title} className='border-2 mt-5' type="text" placeholder='Enter your title' /><br />
                 <label>Enter help box:-</label>
-                <input onChange={(e) => sethelpbox(e.target.value)} className='border-2 mt-5' type="text" value={helpbox} placeholder='Enter your Help Box' /><br />
-                <button onClick={() => handleSubmit} className='bg-amber-300 p-2 ml-10 mt-5'>Sumbit</button>
+                <input onChange={(e) => setdescription(e.target.value)} className='border-2 mt-5' type="text" value={description} placeholder='Enter your Help Box' /><br />
+                <button onClick={() => addtofilterarr} className='bg-amber-300 p-2 ml-10 mt-5 cursor-pointer'>Sumbit</button>
 
                 {filterbton && (
                     <div className="mt-5 ml-10 border-2 w-80 p-5 bg-gray-100">
                         <h3 className="font-bold mb-2">Sumbit Data:</h3>
                         <p>Name:{filterbton.name}</p>
                         <p>Title:{filterbton.title}</p>
-                        <p>Help Box:{filterbton.helpBox}</p>
+                        <p>Help Box:{filterbton.description}</p>
+                        
                     </div>
                 )}
             </div>
